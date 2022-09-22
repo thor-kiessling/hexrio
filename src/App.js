950b8b9e9c2252552c61be0e06e3836fd9949baf
James Brodski <BreathLion@gmail.com>
2022-09-21 19:18:07 -0700

    Added a textbox where users can add SSN.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made by Thor, James, Kevin, Legna, Tim, and Austin.
         </p>
+        <input type="text" placeholder="Enter your SSN"></input>
         <a
           className="App-link"
           href="https://reactjs.org"
