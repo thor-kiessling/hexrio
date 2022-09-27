f9925f10fe0ecd6e098fedcce597b455618c8054
Thor Kiessling <thor.kiessling@gmail.com>
2022-09-27 11:54:00 -0700

    Added a submit ssn button that doesn't do anything.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,7 @@ function App() {
           Made by Thor, James, Kevin, Legna, Tim, and Austin.
         </p>
         <input type="text" placeholder="Enter your SSN"></input>
+        <SSN value = "submit ssn"/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +25,13 @@ function App() {
       </header>
     </div>
   );
+}
+class SSN extends React.Component{
+  render(){
+    return < button >
+      {this.props.value}
+    </button>
+  }
 }
 
 export default App;
