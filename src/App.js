a1ade48292b0723c835344ccc0f50ce0a1dc121f
James Brodski <BreathLion@gmail.com>
2022-10-02 19:04:49 -0700

    Added navigation bar on the top (still needs fixing) Added the "Please do not share your SS# with anyone other than yourself Government sanctioned for your security" text.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import './App.css';
-
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
+
+      <div className="App-navigation">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Seal_of_the_U.S._Department_of_Homeland_Security.svg/1280px-Seal_of_the_U.S._Department_of_Homeland_Security.svg.png" alt="DHS Logo" width="100"></img>
+        Help Home About
+      </div>
+
       <header className="App-header">
-        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Seal_of_the_U.S._Department_of_Homeland_Security.svg/1280px-Seal_of_the_U.S._Department_of_Homeland_Security.svg.png" alt="DHS Logo" width="300"></img></div>
-        U.S. Citizenship and <br></br>Immigration Services
-      </header>
-      <footer className="App-footer">
-        <br></br>
-        <body>
-    
-          <input type="text" placeholder="Enter your SSN" style={{ float: "left", display: "inline" }}></input>
-          <SSN value="🔍" style={{ float: "right", display: "inline" }} />
-          <SSN value="👁" style={{ float: "right", display: "inline" }} />
-          
-        </body>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Seal_of_the_U.S._Department_of_Homeland_Security.svg/1280px-Seal_of_the_U.S._Department_of_Homeland_Security.svg.png" alt="DHS Logo" width="300"></img>
+        <p>U.S. Citizenship and <br></br>Immigration Services</p>
         
+      </header>
+
+      <body className="App-body">
+        <input type="text" placeholder="Enter your SSN" style={{ float: "left", display: "inline" }}></input>
+        <SSN value="👁" style={{}} />
+        <SSN value="🔍" style={{}} />
+      </body>
+
+      <div className="App-body-plus">
+        Please do not share your SS# with anyone other than yourself
+        <br></br>
+        Government sanctioned for your security
         <br></br><br></br><br></br><br></br><br></br><br></br>
-        <p>Why do I have to enter my SS#?</p>
+      </div>
+
+      <footer className="App-footer">
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         <p>
+          Why do I have to enter my SS#?
+          <br></br><br></br>
           This service allows you to retrieve crucial information that would verify your identity.
           This works in collaboration with databases from the DMV, Social Security Department, and Department of State.
         </p>
       </footer>
+
     </div>
   );
 }
