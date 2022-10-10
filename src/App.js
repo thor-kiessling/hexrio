50f167ee9cc478bc2b4b7f3a33e8c1b9cb753acf
Legna Saca <legnajaneth@hotmail.com>
2022-10-10 15:56:46 -0700

    added help page still in progress

--- src/App.js
+++ src/App.js
@@ -2,63 +2,83 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
+        return (
 
-      <div className="App-navigation">
-        <img src="https://www.uscis.gov/sites/default/files/images/site/DHS_cis_W.svg" alt="DHS Logo" width="400"></img>
 
-        <div className="nav2">
-          <a href="google.com">🔍 Help</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="google.com">🏠 Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="google.com">ℹ️ About</a>
-        </div>
-      </div>
+            <div className="App">
 
-      <header className="App-header">
-        <img src="https://www.uscis.gov/sites/default/files/images/article-history/SealsSignatures_USCIS%402x.png" alt="DHS Logo" width="500"></img>
-      </header>
+                <div className="App-navigation">
+                    <img src="https://www.uscis.gov/sites/default/files/images/site/DHS_cis_W.svg" alt="DHS Logo" width="400"></img>
 
-      <body className="App-body">
-        <input type="text" placeholder="Enter your SSN"></input>
-        <SSN value="👁"/>
-        <SSN value="🔍"/>
-      </body>
 
-      <div className="App-body-plus">
-        🛡️ Please do not share your SS# with anyone other than yourself
-        <br></br>
-        ✓ Government sanctioned for your security
+                    <div className="nav2">
+                        <a href="./Help" className="button">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsIaOdsZm0MOEw4Gisgm2D910hdg6Ebr_FkfCG9IzIKp4yGs6m4qHY7CbW420hp2BPxf0&usqp=CAU"
+                                width="20" height="20"></img>Help </a>
+                        <a href="/" className="button">
+                            <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" width="17"
+                                 height="17"></img> Home </a>
+                        <a href='/About' className="button">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/1200px-Magnifying_glass_icon.svg.png"
+                                width="15" height="15"></img>About</a>
 
-      </div>
+                    </div>
+                </div>
 
-      <footer className="App-footer">
+                <header className="App-header">
+                    <img src="https://www.uscis.gov/sites/default/files/images/article-history/SealsSignatures_USCIS%402x.png" alt="DHS Logo" width="500"></img>
+                </header>
 
-        <p>
-          Why do I have to enter my SS#?
-          <br></br><br></br>
-          This service allows you to retrieve crucial information that would verify your identity.
-          This works in collaboration with databases from the DMV, Social Security Department, and Department of State.
-        </p>
-      </footer>
+                <body className="App-body">
+                <input type="text" placeholder="Enter your SSN"></input>
+                <SSN value="👁"/>
+                <SSN value="🔍"/>
+                </body>
 
-    </div>
-  );
-}
-class SSN extends React.Component {
-  render() {
-    return (
-      <div style={{ display: "flex" }}>
-        <button
-          style={{ marginRight: "auto" }}
+                <div className="App-body-plus">
+                    🛡️ Please do not share your SS# with anyone other than yourself
+                    <br></br>
+                    ✓ Government sanctioned for your security
 
-        >
-          {this.props.value}
+                </div>
 
-        </button>
-      </div>
-    )
-  }
-}
+                <footer className="App-footer">
+
+                    <p>
+                        Why do I have to enter my SS#?
+                        <br></br><br></br>
+                        This service allows you to retrieve crucial information that would verify your identity.
+                        This works in collaboration with databases from the DMV, Social Security Department, and
+                        Department of State.
+                    </p>
+                </footer>
+
+            </div>
+
+
+
+        );
+    }
+
+    class SSN extends React.Component {
+        render() {
+            return (
+                <div style={{display: "flex"}}>
+                    <button
+                        style={{marginRight: "auto"}}
+
+                    >
+                        {this.props.value}
+
+                    </button>
+                </div>
+            )
+        }
+    }
+
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Help from "./Help";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+      <Router>
+          <Routes>
+
+              <Route path="/" element={<App/>} />
+              <Route path="/Help" element={<Help/>} />
+
+          </Routes>
+      </Router>
   </React.StrictMode>
 );
 
